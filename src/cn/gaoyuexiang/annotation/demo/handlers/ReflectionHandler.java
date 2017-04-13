81b3ee0734744849106d1d5a5010818312e10e9a
gaoyx <melo@gaoyuexiang.cn>
2017-04-13 11:24:54 +0800

    fixed reflection version

--- src/cn/gaoyuexiang/annotation/demo/handlers/ReflectionHandler.java
+++ src/cn/gaoyuexiang/annotation/demo/handlers/ReflectionHandler.java
@@ -10,9 +10,13 @@ public class ReflectionHandler {
   public void handle(Class clazz) {
     try {
       Annotation annotation = clazz.getAnnotation(ClassNamePrinter.class);
-      System.out.printf("Class is %s", clazz.getName());
+      String clazzName = clazz.getSimpleName();
+      if (annotation == null) {
+        throw new NullPointerException(clazzName);
+      }
+      System.out.printf("Class is %s\n", clazzName);
     } catch (NullPointerException e) {
-      System.out.println("No ClassNamePrinter annotation");
+      System.out.printf("No ClassNamePrinter annotation for %s\n", e.getMessage());
     }
 
   }
